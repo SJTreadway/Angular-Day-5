09600bc7bc12e47a69421f73a987a0825fe1b985
SJTreadway <treadway08@gmail.com>
2015-11-09 16:04:11 -0600

    first commit

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,34 @@
 var app = angular.module('nbaRoutes');
 // the resolved data from the router needs to be injected into the controller
-app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+app.controller('teamCtrl', function ($scope, $stateParams, teamData, teamService) {
+  $scope.teamData = teamData;
+  console.log($scope.teamData)
+  $scope.newGame = {};
+  $scope.showNewGameForm = false;
+  $scope.toggleNewGameForm = function() {
+    $scope.showNewGameForm = !$scope.showNewGameForm;
+  };
+
+  if ($stateParams.team === 'utahjazz') {
+    $scope.homeTeam = 'utahjazz';
+    $scope.logoPath = 'images/jazz-logo.png';
+  } else if ($stateParams.team === 'losangeleslakers') {
+    $scope.homeTeam = 'losangeleslakers';
+    $scope.logoPath = 'images/lakers-logo.png';
+  } else if ($stateParams.team === 'miamiheat') {
+    $scope.homeTeam = 'miamiheat';
+    $scope.logoPath = 'images/heat-logo.png';
+  }
+
+  $scope.submitGame = function() {
+    $scope.newGame.homeTeam = $scope.homeTeam.split('').join('').toLowerCase();
+    teamService.addNewGame($scope.newGame)
+      .then(teamService.getTeamData($scope.newGame.homeTeam))
+      .then(function(data) {
+        $scope.teamData = data;
+        $scope.newGame = {};
+        $scope.showNewGameForm = false;
+      });
+  }
 
 });
